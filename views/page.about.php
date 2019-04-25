76f9f8eb644bda94a82edb95341a1d7ca26b77e2
Axel <ax.fiolle@gmail.com>
2019-04-25 14:30:09 +0200

    cards and grids

--- views/page.about.php
+++ views/page.about.php
@@ -18,46 +18,100 @@
     <h2>Trip to knowledge</h2>
     <h3>1. Being at the right place at the right moment</h3>
     <div id="mozaicTrigger"></div>
-    <div id="journey" class="grid two-c journey">
-      <div class="skill-tile nomaxhskill jpacity"><h4>Junior Web Developer at BeCode, 2017-2018</h4><img class="ico small" src="<?= IMG.'ico/becodeorg.png' ?>" alt="BeCode"></div>
-      <div class="skill-tile nomaxhskill jpacity hidden">Co-founder of <strong>SmartEntrepreneuriat</strong></div>
-      <div class="skill-tile nomaxhskill jpacity hidden">Co-founder of <strong>l'Agence du Web</strong></div>
-      <div class="skill-tile nomaxhskill jpacity"><h4>Studied at Albert Jacquard, 2013 to 2016</h4><img class="ico small" src="<?= IMG.'ico/heaj.png' ?>" alt="HEAJ"></div>
+    <div class="container">
+      <div id="journey" class="row journey">
+        <div class="col s12 m6">
+          <div class="card skill-tile nomaxhskill jpacity">
+            <div class="card-content">
+              <h4>Junior Web Developer at BeCode, 2017-2018</h4><img class="ico small" src="<?= IMG.'ico/becodeorg.png' ?>" alt="BeCode">
+            </div>
+          </div>
+        </div>
+        <div class="col s12 m6">
+          <div class="card skill-tile nomaxhskill jpacity">
+            <div class="card-content">
+              <h4>Studied at Albert Jacquard, 2013 to 2016</h4><img class="ico small" src="<?= IMG.'ico/heaj.png' ?>" alt="HEAJ">
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <h3 class="clearfix">2. Start from scratch</h3>
-    <div id="scratch" class="grid three-c learned">
-      <div class="skill-tile">
-        <i class="devicon-html5-plain devicon big"><span class="alt-tile"><strong>HTML5</strong> warrior</span></i>
-      </div>
-      <div class="skill-tile">
-        <i class="devicon-css3-plain devicon big"><span class="alt-tile"><strong>CSS3</strong> wizard</span></i>
-      </div>
-      <div class="skill-tile">
-        <i class="devicon-javascript-plain devicon big"><span class="alt-tile"><strong>JavaScript</strong> writer</span></i>
+    <div class="container">
+      <div id="scratch" class="row learned">
+        <div class="col s12 m4">
+          <div class="card skill-tile">
+          <div class="card-content">
+            <i class="devicon-html5-plain devicon big"></i><span class="alt-tile"><strong>HTML5</strong> warrior</span>
+          </div>
+          </div>
+        </div>
+        <div class="col s12 m4">
+          <div class="card skill-tile">
+          <div class="card-content">
+            <i class="devicon-css3-plain devicon big"></i><span class="alt-tile"><strong>CSS3</strong> wizard</span>
+          </div>
+          </div>
+        </div>
+        <div class="col s12 m4">
+          <div class="card skill-tile">
+          <div class="card-content">
+            <i class="devicon-javascript-plain devicon big"></i><span class="alt-tile"><strong>JavaScript</strong> writer</span>
+          </div>
+          </div>
+        </div>
       </div>
     </div>
     <h3 class="clearfix">3. Learn frameworks</h3>
-    <div id="frameworks" class="grid three-c learned">
-      <div class="skill-tile">
-        <i class="fas fa-code devicon big"><span class="alt-tile">Worked professionnally with <strong>PolymerJS</strong></span></i>
-      </div>
-      <div class="skill-tile">
-        <i class="devicon-react-plain devicon big"><span class="alt-tile">Worked on multiple <strong>React</strong> projects</span></i>
-      </div>
-      <div class="skill-tile">
-        <i class="fab fa-angular devicon big"><span class="alt-tile">Started <strong>Angular</strong> a few moments ago</span></i>
+    <div class="container">
+      <div id="frameworks" class="row learned">
+        <div class="col s12 m4">
+          <div class="card skill-tile">
+          <div class="card-content">
+            <i class="fas fa-code devicon big"></i><span class="alt-tile">Worked professionnally with <strong>PolymerJS</strong></span>
+          </div>
+          </div>
+        </div>
+        <div class="col s12 m4">
+          <div class="card skill-tile">
+          <div class="card-content">
+            <i class="devicon-react-plain devicon big"></i><span class="alt-tile">Worked on multiple <strong>React</strong> projects</span>
+          </div>
+          </div>
+        </div>
+        <div class="col s12 m4">
+          <div class="card skill-tile">
+          <div class="card-content">
+            <i class="fab fa-angular devicon big"></i><span class="alt-tile">Started <strong>Angular</strong> a few moments ago</span>
+          </div>
+          </div>
+        </div>
       </div>
     </div>
     <h3 class="clearfix">4. Get more skills</h3>
-    <div id="more" class="grid three-c learned">
-      <div class="skill-tile">
-        <i class="devicon-php-plain devicon big"><span class="alt-tile"><a href="https://www.linkedin.com/in/axel-fiolle/detail/skills/(ACoAABP99YMBYi-m_8zbKoUSOzyNZBdZ7eUiMlw,28)/">Everybody</a> says I'm good at <strong>PHP</strong></span></i>
-      </div>
-      <div class="skill-tile">
-        <i class="devicon-nodejs-plain devicon big"><span class="alt-tile"><strong>NodeJS</strong> was in my sci-fi dreams</span></i>
-      </div>
-      <div class="skill-tile">
-        <i class="fas fa-graduation-cap devicon big"><span class="alt-tile">Keep learning <strong>since 2005</strong></span></i>
+    <div class="container">
+      <div id="more" class="row learned">
+        <div class="col s12 m4">
+          <div class="card skill-tile">
+          <div class="card-content">
+            <i class="devicon-php-plain devicon big"></i><span class="alt-tile"><a href="https://www.linkedin.com/in/axel-fiolle/detail/skills/(ACoAABP99YMBYi-m_8zbKoUSOzyNZBdZ7eUiMlw,28)/">Everybody</a> says I'm good at <strong>PHP</strong></span>
+          </div>
+          </div>
+        </div>
+        <div class="col s12 m4">
+          <div class="card skill-tile">
+          <div class="card-content">
+            <i class="devicon-nodejs-plain devicon big"></i><span class="alt-tile"><strong>NodeJS</strong> was in my sci-fi dreams</span>
+          </div>
+          </div>
+        </div>
+        <div class="col s12 m4">
+          <div class="card skill-tile">
+          <div class="card-content">
+            <i class="fas fa-graduation-cap devicon big"></i><span class="alt-tile">Keep learning <strong>since 2005</strong></span>
+          </div>
+          </div>
+        </div>
       </div>
     </div>
     <a href="./?page=portfolio" class="btn shadebtn clearfix">My portfolio</a>
