21a003e28ca01dbbd65f27da363947483c70cf27
Axel <ax.fiolle@gmail.com>
2019-12-15 11:46:45 +0100

    [IMP] Improved the code
    + Usage of EcmaScript
    + Usage of async functions to call mail api
    + Turned some func() to oneLiner
    - Removed Slick global code

--- views/assets/js/App/app.js
+++ views/assets/js/App/app.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
+  // Setting needed global const
+  const PAGE_INFOS = {
+    url: window.location.href || document.URL,
+    name: (window.location.href || document.URL).slice((window.location.href || document.URL).search('page-')+5,(window.location.href || document.URL).length)
+  };
   // AT START ---------------------------------------
-  let vpHeight = $(window).height();
-  let vpWidth = $(window).width();
-  let docHeight = $(document).height();
+  const docHeight = $(document).height();
   let scrollTop = $(document).scrollTop();
   $(".jhide").addClass("hidden");
   $(".jpacity").addClass("nopacity");
@@ -14,57 +17,39 @@ $(document).ready(function(){
     $("footer").removeClass("hidden");
   }
   // FUNCTIONS ---------------------------------------
-  function scrollTo(target,speed,fade) {
-    const targetPos = $(target).offset().top;
-    $("html, body").animate(
-      {
-        scrollTop: targetPos
-      }, speed
-    ).fadeIn(fade);
-  }
-  function validateEmail(email) {
-    const regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-		return regex.test(email);
-  }
-  function sendMail(name,mail,msg) {
+  const scrollTo = (target,speed,fade) => $("html, body").animate({scrollTop: $(target).offset().top}, speed).fadeIn(fade);
+  const validateEmail =(email) => /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email);
+  const sendMail = async (name,mail,msg) => {
     console.log('sendMail(name,mail,msg)')
-    if (!validateEmail(mail)) return false
     if (name && mail && msg && validateEmail(mail)) {
-      const sendBtnContent = document.querySelector('#sendmail').innerHTML
-      document.querySelector('#sendmail').innerHTML = 'Sending ...'
+      const sendBtnContent = document.querySelector('#sendmail').innerHTML;
+      const sendBtn = document.querySelector('#sendmail');
+      sendBtn.innerHTML = 'Sending ...';
       const xhr = new XMLHttpRequest();
+      let resMsg = '', mailTo = '';
+      xhr.onreadystatechange = function() {
+        console.warn(`Returned response: ${ xhr.responseText }, ${ xhr.statusText }`);
+        if(xhr.readyState == 4 && xhr.status === 200) resMsg = 'Thanks for your confidence ! Your message has been sent to the mailman :)';
+        else resMsg = 'I\'m sorry but it didn\'t work. You can join me at this adress instead :&nbsp;<a href="mailto:ax.fiolle@gmail.com">ax.fiolle@gmail.com</a>';
+      }
       xhr.open('POST', 'https://axelfiolle.be/api/mail.php', false); 
       xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-      let resMsg = ''
-      let mailTo = ''
-
-      xhr.onreadystatechange = function() {
-        if(xhr.readyState == 4 && xhr.status === 200) {
-          console.log("Réponse reçue: %s", xhr.responseText);
-          resMsg = 'Thanks for your confidence ! Your message has been sent to the mailman :)';
-        } else {
-          console.warn("Status de la réponse: %d (%s)", xhr.status, xhr.statusText);
-          resMsg = 'I\'m sorry but it didn\'t work. You can join me at this adress instead : ax.fiolle@gmail.com';
-        }
-        M.toast({
-          displayLength: 6000,
-          html: resMsg
-        })
-        document.querySelector('#sendmail').innerHTML = sendBtnContent
-      }
-
-      xhr.send(`name=${name}&mail=${mail}&msg=${msg}`);
-
-    } else {
+      await xhr.send(`name=${name}&mail=${mail}&msg=${msg}`);
+      M.toast({ displayLength: 6000, html: resMsg });
+    } else if (!name || !mail || !msg || (!name.length > 0) || (!mail.length > 0) || (!msg.length > 0)) {
+      resMsg = "You missed some fields";
+      M.toast({ displayLength: 6000, html: resMsg });
       console.warn('sendMail('+name+','+mail+','+msg+')=> undefined param')
+    } else if (!validateEmail(mail)) {
+      resMsg = "Invalid email";
+      M.toast({ displayLength: 6000, html: resMsg });
+      console.warn('sendMail('+name+','+mail+','+msg+')=> Email is not valid')
     }
   }
   // SCROLLS --------------------------------------- 
-  $(document).scroll(function(e) {
+  $(document).scroll((e) => {
     $("span.and").addClass("rotate");
     scrollTop = $(document).scrollTop();
-    // console.log("scrollTop : "+scrollTop);
     if (scrollTop <= 100) {
       $("#landingscroll").removeClass("hide");
       $("footer").addClass("hidden");
@@ -72,104 +57,42 @@ $(document).ready(function(){
       $("#landingscroll").addClass("hide");
       $("footer").removeClass("hidden");
     }
-    if ($("#mozaicTrigger").visible(true)){
-      $(".skill-tile").removeClass("nopacity");
-    }
-    // $("#visitlink").close();
+    if ($("#mozaicTrigger").visible(true)) $(".skill-tile").removeClass("nopacity");
   });
   // CLICKS ---------------------------------------
-  $(".scrollback").click(function(e) {
+  $(".scrollback").click((e) => {
     e.preventDefault();
     const target = $(this).attr("href");
     scrollTo(target,1000,500);
-  })
-  /* $("#visitlink").click(function(e) {
-    e.preventDefault();
-    $("#visitmenu").toggleClass("shown");
-    $("span.and").toggleClass("rotate");
-  }); */
-  $(".contactlink").click(function(e){
+  });
+  $(".contactlink").click((e) => {
     e.preventDefault();
     $("html, body").animate({scrollTop: docHeight}, 2500).fadeIn(1000);
   });
-  $("#errorclose").click(function(e) {
+  $("#errorclose").click((e) => {
     e.preventDefault();
     $("#mailtab").addClass("hidden");
   });
-  $("#sendmail").click((e)=>{
-    // console.log('$("#sendmail").click')
+  $("#sendmail").click((e) => {
     e.preventDefault();
-    const name = $("#nameinput")[0].value
-    const mail = $("#mailinput")[0].value
-    const msg = $("#msgmail")[0].value
-    if (name && mail && msg && name.length && mail.length && msg.length) {
-      sendMail(name,mail,msg)
-    }
-  })
+    const name = $("#nameinput")[0].value, mail = $("#mailinput")[0].value, msg = $("#msgmail")[0].value;
+    if (name && mail && msg && name.length && mail.length && msg.length) sendMail(name,mail,msg);
+  });
   $("#visitlink").dropdown();
   $('.collapsible').collapsible();
   $('.materialboxed').materialbox();
-
   // FORM ---------------------------------------
-  $("#mailform > *").keyup(function(){
-    if (
-      ($("#nameinput").val() != '') &&
-      ($("#mailinput").val() != '') &&
-      ($("#msgmail").val() != '') &&
-      (validateEmail($("#mailinput").val()))
-    ) {
+  $("#mailform > *").keyup(() => {
+    if ( ($("#nameinput").val() != '') && ($("#mailinput").val() != '') && ($("#msgmail").val() != '') && (validateEmail($("#mailinput").val())) ) {
       $("#sendmail").addClass("enabled");
       $("#sendmail").addClass("pulse");
       $("#sendmail").prop("disabled",false);
       $("#sendmail").removeProp("disabled");
-      // console.log("yup");
     } else {
       $("#sendmail").removeClass("enabled");
       $("#sendmail").removeClass("pulse");
       $("#sendmail").prop("disabled",true);
-      // console.log("nope");
     }
   });
-  $("#msgmail").keydown(()=>{
-    // console.log('Ouïlle')
-    setTimeout(()=>$("#msgmail")[0].style.height = $("#msgmail")[0].scrollHeight + 'px',0)
-  })
-  // HANDLE ---------------------------------------
-  /*$("#msgmail").resize(function(){
-    $(".section:last-child").css("margin-bottom",$("#footer").heigh());
-  });*/
-  // SLICK ---------------------------------------
-  //websites
-  $("#webslide").slick({
-    dots: true,
-    infinite: true,
-    centerMode: true,
-    slidesToShow: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
-  //photo 
-  $("#viewphotoslide").carousel({
-    fullWidth: true,
-    numVisible: 1,
-    indicators: true
-  });
-  /* $("#viewphotoslide").slick({
-    dots: true,
-    infinite: true,
-    centerMode: true,
-    arrows: false,
-    slidesToShow: 1,
-    asNavFor: '#photoslide',
-  }); */
-  $("#photoslide").slick({
-    dots: false,
-    infinite: true,
-    centerMode: true,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    slidesToShow: 3,
-    asNavFor: '#viewphotoslide',
-  });
-  
-});
+  $("#msgmail").keydown(() => setTimeout(()=>$("#msgmail")[0].style.height = $("#msgmail")[0].scrollHeight + 'px',0));
+}); // ready end
