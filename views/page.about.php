a1758a71e76663c75e801b2533081a38e9ba4cde
Axel <ax.fiolle@gmail.com>
2019-12-15 01:59:17 +0100

    Replaced all tile's div by article
    Mostly for code readability

--- views/page.about.php
+++ views/page.about.php
@@ -20,105 +20,105 @@
     <div id="mozaicTrigger"></div>
     <div class="container">
       <div id="journey" class="row journey">
-        <div class="col s12 m6 offset-m3">
+        <article class="col s12 m6 offset-m3">
           <div class="card skill-tile dark-nice">
           <div class="card-content">
             <h4>Lead developer at <a href="https://timbtrack.com/"><abbr title="International Business Renaissance">IBR</abbr></a></h4><img src="<?= IMG.'/ico/logo-timbtrack-block-white.png' ?>" alt="Timbtrack" class="ico small">
           </div>
           </div>
-        </div>
-        <div class="col s12 m6">
+        </article>
+        <article class="col s12 m6">
           <div class="card skill-tile nomaxhskill">
             <div class="card-content">
               <h4>Mixed all knowledge at BeCode, 2017-2018</h4><img class="ico small" src="<?= IMG.'ico/becodeorg.png' ?>" alt="BeCode">
             </div>
           </div>
-        </div>
-        <div class="col s12 m6">
+        </article>
+        <article class="col s12 m6">
           <div class="card skill-tile nomaxhskill">
             <div class="card-content">
               <h4>Studied at Albert Jacquard, 2013 to 2016</h4><img class="ico small" src="<?= IMG.'ico/heaj.png' ?>" alt="HEAJ">
             </div>
           </div>
-        </div>
+        </article>
       </div>
     </div>
     <h3 class="clearfix">2. Start from scratch</h3>
     <div class="container">
       <div id="scratch" class="row learned">
-        <div class="col s12 m4">
+        <article class="col s12 m4">
           <div class="card skill-tile">
           <div class="card-content">
             <i class="devicon-html5-plain devicon big"></i><span class="alt-tile"><strong>HTML5</strong> warrior</span>
           </div>
           </div>
-        </div>
-        <div class="col s12 m4">
+        </article>
+        <article class="col s12 m4">
           <div class="card skill-tile">
           <div class="card-content">
             <i class="devicon-css3-plain devicon big"></i><span class="alt-tile"><strong>CSS3</strong> wizard</span>
           </div>
           </div>
-        </div>
-        <div class="col s12 m4">
+        </article>
+        <article class="col s12 m4">
           <div class="card skill-tile">
           <div class="card-content">
             <i class="devicon-javascript-plain devicon big"></i><span class="alt-tile"><strong>JavaScript</strong> writer</span>
           </div>
           </div>
-        </div>
+        </article>
       </div>
     </div>
     <h3 class="clearfix">3. Learn frameworks</h3>
     <div class="container">
       <div id="frameworks" class="row learned">
-        <div class="col s12 m4">
+        <article class="col s12 m4">
           <div class="card skill-tile">
           <div class="card-content">
             <i class="fab fa-vuejs devicon big"></i><span class="alt-tile">Works actively on multiple <strong>VueJS</strong> <a href="https://timbtrack.com/">projects</a></span>
           </div>
           </div>
-        </div>
-        <div class="col s12 m4">
+        </article>
+        <article class="col s12 m4">
           <div class="card skill-tile">
           <div class="card-content">
             <i class="devicon-react-plain devicon big"></i><span class="alt-tile">Worked on several <strong>React</strong> <a href="https://constructr.be/">applications</a></span>
           </div>
           </div>
-        </div>
-        <div class="col s12 m4">
+        </article>
+        <article class="col s12 m4">
           <div class="card skill-tile">
           <div class="card-content">
             <i class="fab fa-angular devicon big"></i><span class="alt-tile">Can handle <strong>Angular</strong> with ease</span>
           </div>
           </div>
-        </div>
+        </article>
       </div>
     </div>
     <h3 class="clearfix">4. Get more skills</h3>
     <div class="container">
       <div id="more" class="row learned">
-        <div class="col s12 m4">
+        <article class="col s12 m4">
           <div class="card skill-tile">
           <div class="card-content">
             <i class="devicon-php-plain devicon big"></i><span class="alt-tile"><a href="https://www.linkedin.com/in/axel-fiolle/detail/skills/(ACoAABP99YMBYi-m_8zbKoUSOzyNZBdZ7eUiMlw,28)/">Everybody</a> says I'm good at <strong>PHP</strong></span>
           </div>
           </div>
-        </div>
-        <div class="col s12 m4">
+        </article>
+        <article class="col s12 m4">
           <div class="card skill-tile">
           <div class="card-content">
-            <i class="devicon-nodejs-plain devicon big"></i><span class="alt-tile"><strong>NodeJS</strong> was in my sci-fi dreams</span>
+            <i class="devicon-nodejs-plain devicon big"></i><span class="alt-tile"><strong>NodeJS</strong> is the coolest thing I ever handled</span>
           </div>
           </div>
-        </div>
-        <div class="col s12 m4">
+        </article>
+        <article class="col s12 m4">
           <div class="card skill-tile">
           <div class="card-content">
             <i class="fas fa-server devicon big"></i><span class="alt-tile"><strong>MySQL</strong> and <strong>MongoDB</strong> are my first choices</span>
           </div>
           </div>
-        </div>
+        </article>
       </div>
     </div>
     <a href="./page-portfolio" class="btn waves-effect shadebtn clearfix">My portfolio</a>
