bc33874bc4e8ee648189f54a1736f461124fc639
Anda <ax.fiolle@gmail.com>
2020-05-01 19:47:33 +0200

    surfix

--- api/mail.php
+++ api/mail.php
@@ -3,7 +3,7 @@
 if ("POST" == $_SERVER["REQUEST_METHOD"]) {
     if (isset($_SERVER["HTTP_ORIGIN"])) {
         $address = "https://axelfiolle.be";
-        if (strpos($address, $_SERVER["HTTP_ORIGIN"]) !== 0) {
+        if ($address !== $_SERVER["HTTP_ORIGIN"]) { // if (strpos($address, $_SERVER["HTTP_ORIGIN"]) !== 0) {
             exit("CSRF protection in POST request: detected invalid Origin header: ".$_SERVER["HTTP_ORIGIN"]);
         } else {
             if( 
